d3c0457d2179d9ae01070220a0cda0aa62e57acf
Hyden Polikoff <hyden.dev@gmail.com>
2022-09-22 13:25:59 -0600

    cleanup

--- lab4_driver.py
+++ lab4_driver.py
@@ -1,14 +1,63 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 import mypkg
 
+
+def compute_order(x, xstar):
+    # p_{n+1}-p (from the second index to the end)
+    diff1 = np.abs(x[1::] - xstar)
+    # p_n-p (from the first index to the second to last)
+    diff2 = np.abs(x[0:-1] - xstar)
+    # linear fit to log of differences
+    fit = np.polyfit(np.log(diff2.flatten()), np.log(diff1.flatten()), 1)
+    print('the order equation is')
+    print('log(|p_{n+1}-p|) = log(lambda) + alpha*log(|p_n-p|) where')
+    print('lambda = ' + str(np.exp(fit[1])))
+    print('alpha = ' + str(fit[0]))
+    return [fit, diff1, diff2]
+
+
+def aitkens(x, xstar, tol):
+    count = len(x)
+    hat_x = np.zeros((count - 2, 1))
+    for n in range(0, count - 2):
+        # compute new iterates using aitkens formula
+        hat_x[n] = x[n] - (x[n + 1] - x[n]) ** 2 / (x[n + 2] - 2 * x[n + 1] + x[n])
+        # return if we're less than tol
+        if np.abs(hat_x[n] - xstar) < tol:
+            return hat_x[0:n + 1]
+    return hat_x
+
+
+########################################################
 if __name__ == '__main__':
-    # instantiate Iteration1D object using bisection method
-    find = mypkg.Iteration1D(lambda x: np.power(10 / (x + 4), 1 / 2), 'fixedpt')
+
+    find = mypkg.Iteration1D(lambda x: np.power(10 / (x + 4), 1.0 / 2.0), 'fixedpt')
     find.p0 = 1.5
     find.tol = 1e-10
     find.Nmax = 100
 
     find.root()
-    find.compute_order()
-    find.plot_fit()
+
+    xstar = find.pstar
+    x = find.fixed_pt_approximations
+
+    hat_x = aitkens(x, xstar, 1e-10)
+
+    # compute the convergence rate and constant
+    print('\nrate and constants for fixedpt:')
+    [fit, diff1, diff2] = compute_order(x, xstar)
+    print('\nrate and constants for aitkens')
+    [fit1, diff11, diff21] = compute_order(hat_x, xstar)
+    # plot the data
+    plt.loglog(diff2, diff1, 'ro', label='fixedpt data')
+    plt.loglog(diff21, diff11, 'gx', label='aitkens data')
+    # plot the fits
+    plt.loglog(diff2, np.exp(fit[1] + fit[0] * np.log(diff2)), 'r-', label='fixedpt fit')
+    plt.loglog(diff21, np.exp(fit1[1] + fit1[0] * np.log(diff21)), 'g-', label='aitkens fit')
+    # label the plot axes and create legend
+    plt.xlabel('$|p_{n}-p|$')
+    plt.ylabel('$|p_{n+1}-p|$')
+    plt.legend()
+    plt.show()
--- mypkg/Iteration1D.py
+++ mypkg/Iteration1D.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 
 
 class Iteration1D:
@@ -10,7 +9,7 @@ class Iteration1D:
         # initial interval for bisection:
         self.a = None
         self.b = None
-        # initial guess for newton/fixedpt
+        # initial guess for newton/fixed point
         self.p0 = None
         # tolerance and max iter
         self.tol = None
@@ -19,66 +18,27 @@ class Iteration1D:
         self.info = None
         # root
         self.pstar = None
-        # iters for newton or fixedpt
+        # iters for newton or fixed point
         self.p_iters = None
         # the vector of approximations made in fixed point iteration
         self.fixed_pt_approximations = None
-        self.fit = None
-        self.diff1 = None
-        self.diff2 = None
 
     def root(self) -> None:
         if self.method == 'bisection':
             # add checks and bisection routine
             if self.f is None or self.a is None or self.b is None or self.tol is None or self.Nmax is None:
-                print('error: some attributes for bisection aresys. returning ..')
-                return
+                raise Exception('error: some attributes for bisection aresys. returning ..')
 
             self.pstar, self.info = bisection(self.f, self.a, self.b, self.tol, self.Nmax)
         elif self.method == 'fixedpt':
             # add checks and fixed point routine
             if self.f is None or self.p0 is None or self.tol is None or self.Nmax is None:
-                print('error: some attributes for fixedpt are unset. returning ..')
-                return
+                raise Exception('error: some attributes for fixedpt are unset. returning ..')
 
-            self.pstar, self.fixed_pt_approximations, self.info = fixedpt_with_approximations(self.f, self.p0, self.tol,
+            self.pstar, self.fixed_pt_approximations, self.info = fixed_point(self.f, self.p0, self.tol,
                                                                                               self.Nmax)
-
-    def compute_order(self) -> None:
-        """
-        Note that a fixed point iterations must be run before computing the order
-        """
-
-        if self.fixed_pt_approximations is None:
-            raise ValueError("fixed_pt_approximations - must run fixed point iteration before computing order")
-
-        # p_{n+1}-p (from the second index to the end)
-        self.diff1 = np.abs(self.fixed_pt_approximations[1::] - self.pstar)
-        # p_n-p (from the first index to the second to last)
-        self.diff2 = np.abs(self.fixed_pt_approximations[0:-1] - self.pstar)
-
-        # linear fit to log of differences
-        self.fit = np.polyfit(np.log(self.diff2.flatten()), np.log(self.diff1.flatten()), 1)
-
-        print('the order equation is')
-        print('log(|p_{n+1}-p|) = log(lambda) + alpha*log(|p_n-p|) where')
-        print('lambda = ' + str(np.exp(self.fit[1])))
-        print('alpha = ' + str(self.fit[0]))
-
-    def plot_fit(self) -> None:
-        """
-        Fixed point and order computation before plotting the fit
-        """
-        if self.fit is None:
-            raise ValueError("calculated fit is None - must run order calculation plotting fit")
-
-        plt.loglog(self.diff2, self.diff1, 'ro', label='data')
-        # plot the fit
-        plt.loglog(self.diff2, np.exp(self.fit[1] + self.fit[0] * np.log(self.diff2)), 'b-', label='fit')
-        plt.xlabel('$|p_{n}-p|$')
-        plt.ylabel('$|p_{n+1}-p|$')
-        plt.legend()
-        plt.show()
+        elif self.method == 'newton':
+            raise NotImplemented
 
 
 def bisection(f, a, b, tol, Nmax):
@@ -147,32 +107,12 @@ def bisection(f, a, b, tol, Nmax):
     return [astar, ier]
 
 
-def fixedpt(f, x0, tol, Nmax):
+def fixed_point(f, x0, tol, Nmax):
     """x0 = initial guess"""
     """Nmax = max number of iterations"""
     """tol = stopping tolerance"""
 
-    count = 0
-    while count < Nmax:
-        count = count + 1
-        x1 = f(x0)
-        if abs(x1 - x0) < tol:
-            xstar = x1
-            ier = 0
-            return [xstar, ier]
-        x0 = x1
-
-    xstar = x1
-    ier = 1
-    return [xstar, ier]
-
-
-def fixedpt_with_approximations(f, x0, tol, Nmax):
-    """x0 = initial guess"""
-    """Nmax = max number of iterations"""
-    """tol = stopping tolerance"""
-
-    approximations = np.zeros((Nmax, 1))
+    approximations = np.zeros((Nmax+1, 1))
 
     count = 0
     while count < Nmax:
