f12e941cee0d84718cd9965e5ee85a6b1f9e1bc8
Hyden Polikoff <hyden.dev@gmail.com>
2022-11-03 10:47:56 -0600

    more homework 10 work

--- homework/homework10/problem1.py
+++ homework/homework10/problem1.py
@@ -11,10 +11,16 @@ if __name__ == '__main__':
     A = -5
     B = 5
     N = 20
+    EXACT_VALUE = 2.746801533890032
 
     # part A
-    print('trapezoidal =', trapezoidal(f, A, B, N))
-    print('simpson =', composite_simpson(f, A, B, N))
+    trap_result = trapezoidal(f, A, B, N)
+    simp_result = composite_simpson(f, A, B, N)
+    print('Results with N = 20')
+    print('trapezoidal =', trap_result)
+    print('trapezoidal error =', abs(trap_result - EXACT_VALUE))
+    print('simpson =', simp_result)
+    print('simpson error =', abs(simp_result - EXACT_VALUE))
     # results:
     # trapezoidal = 2.7462081624602193
     # simpson = 2.7429080175186034
@@ -22,9 +28,29 @@ if __name__ == '__main__':
     print()
 
     # part C
-    EPSILON = 10e-4
+    EPSILON = 10e-8
 
-    _, _, info_dict = quad(f, A, B, full_output=1)
+    prev_result = float('inf')
+    for i in range(5, 1_000):
+        trap_result = trapezoidal(f, A, B, i)
+        if abs(prev_result - trap_result) < EPSILON:
+            print(f'Trapezoid requires {i} intervals - for e = {EPSILON}')
+            break
+        prev_result = trap_result
+
+    for i in range(5, 1_000):
+        simp_result = trapezoidal(f, A, B, i)
+
+        if abs(prev_result - simp_result) < EPSILON:
+            print(f'Simpson requires {i} intervals - for e = {EPSILON}')
+            break
+        prev_result = trap_result
+
+    result, _, info_dict = quad(f, A, B, full_output=1)
     print('neval for scipy quad with default tolerance:', info_dict['neval'])
-    _, _, info_dict = quad(f, A, B, epsabs=EPSILON, full_output=1)
+    print('difference to trapezoid result =', abs(result - trap_result))
+    print('difference to simpson result =', abs(result - simp_result))
+    result, _, info_dict = quad(f, A, B, epsabs=EPSILON, full_output=1)
     print('neval for scipy quad with error of 10e-4:', info_dict['neval'])
+    print('difference to trapezoid result =', abs(result - trap_result))
+    print('difference to simpson result =', abs(result - simp_result))
